57ac55ac86b1fc5c515546c95a786d2e44c3a876
Berger Lima <berg_lima@live.com>
2024-09-16 06:34:20 -0300

    adicionada aula de depuracao ao repositorio

--- Basico/teste-depuracao/index.js
+++ Basico/teste-depuracao/index.js
@@ -1,5 +1,22 @@
-console.log("Ricardo");
+function calcularFatorial(numero){
+    let resultado = 1;
 
-console.log("Ricardo");
+    for(let i = 1; i <= numero; i++){
+        resultado *= i;
+    }
 
-console.log("Ricardo");
+    return resultado;
+}
+
+function imprimirFatorial(numero) {
+
+    if(numero >= 0){
+        const fatorial = calcularFatorial(numero);
+        console.log(`O fatorial de ${numero} é ${fatorial}`)
+    } else {
+        console.log("Não é possível calcular o fatorial e um número negativo.");        
+    }
+}
+
+const numero = 5;
+imprimirFatorial(numero);
